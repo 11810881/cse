af01c75d468fb0280fd54790ade818bf97e42a44
jatin singh <63221473+11810881@users.noreply.github.com>
2020-04-10 15:50:09 +0530

    create scheduler
    initial values are taken ,total no of process are scanned and the burst time of each process is scanned

---
+++ scheduler.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int bt[10],p[10],n,temp,i,j,wt[10],sum=0;
+    float avg;
+    printf("Enter total no of proces:");
+    scanf("%d",&n);
+    printf("\n Enter burst time for each process:-");
+    for(i=0;i<n;i++)
