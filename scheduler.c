ec7ca9e9eeea3b1b04132f4e05c9d0ffff17e550
jatin singh <63221473+11810881@users.noreply.github.com>
2020-04-12 10:57:53 +0530

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -13,3 +13,19 @@ int main()
         scanf("%d",&bt[i]);
         p[i]=i;
     }
+}
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[i]>bt[j])
+            {
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
